13e29821d5f76e76078e77fc52c9183db0155a47
Muhammad Iqbaal <m.iqbaal2803@gmail.com>
2021-05-07 08:43:58 +0700

    update

--- app/Http/Controllers/TagihanController.php
+++ app/Http/Controllers/TagihanController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\AppHelper;
-use App\Models\tagihan;
+use App\Models\Tagihan;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use CoreComponentRepository;
