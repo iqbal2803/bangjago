b25d01469b359be96ee57b664a5f17c2a3372d6a
Muhammad Iqbaal <m.iqbaal2803@gmail.com>
2021-05-10 09:32:32 +0700

    update cabang

--- resources/views/cabang/edit_cabang.blade.php
+++ resources/views/cabang/edit_cabang.blade.php
@@ -129,7 +129,7 @@
                         </div>
                       </div>
                       <div class="col-sm-2">
-                        <select class="form-control" name="staff_id" id="staff_id">
+                        <select class="form-control" name="users_id" id="users_id">
                           @foreach ($datauserscabang as $data)
                           <option value="{{$data->id}}" @if ($cabang->users_id==$data->id) selected @endif >{{$data->name}}</option>
                           @endforeach
