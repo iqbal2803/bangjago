5277603c43b71c5276a787fb15aa5c452585d808
Muhammad Iqbaal <m.iqbaal2803@gmail.com>
2021-06-11 15:49:11 +0700

    upp

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::get('getProfil', 'ProfilController@getProfil');
 Route::group(['namespace' => 'api'], function () {
 
-	Route::group(['prefix' => 'profil'], function () {
-			Route::get('getProfil', 'ProfilController@getProfil');
-	});
 
 	Route::group(['prefix' => 'auth'], function () {
 		Route::post('login', 'AuthController@login');
